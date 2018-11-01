36e1883fc354b59ad42bf8dc22b849693e6cebb6
Alexander Whipp <alexruns4@gmail.com>
2018-11-01 15:47:51 -0400

    Update cookieStore.js

--- cookieStore.js
+++ cookieStore.js
@@ -4,7 +4,7 @@
         var d = new Date();
         d.setTime(d.getTime() + (exdays*24*60*60*1000));
         var expires = "expires="+ d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        document.cookie = cname + "=" + cvalue + "; expires=" + expires;
     };
 
     function cookieStoreGet(cname) {
